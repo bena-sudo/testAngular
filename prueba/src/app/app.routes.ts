4dfc4c363fe8bf2d9634d14b2fa5066b2cf0da6e
bena-sudo <joanbenaventcastells@gmail.com>
2025-03-06 23:20:35 +0100

    apuntes

--- prueba/src/app/app.routes.ts
+++ prueba/src/app/app.routes.ts
@@ -6,3 +6,6 @@ export const routes: Routes = [
   { path: '', component: MapaComponent },
   { path: 'idioma', component: IdomasComponent },
 ];
+
+// apuntes: https://www.codeandweb.com/babeledit/tutorials/how-to-translate-your-angular-app-with-ngx-translate#install-standalone
+// npm install @ngx-translate/core @ngx-translate/http-loader @colsen1991/ngx-translate-extract-marker
